7454cbd3c2e428c083ccffb01b058128248375dd
yisas93 <juan249325@gmail.com>
2022-11-25 19:27:10 +0000

    todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React,{useState} from "react";
+import List from "./card.jsx"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tasks, setTasks]= useState([
+		
+	])
+	const [newTask, setNewTask]=useState("")
+	function addTask(e){
+		if(e.code=="Enter" && newTask!=""){
+			setTasks([...tasks, newTask])
+			setNewTask("")
+		}
+	}
+	
+	function removeTask (index){
+		let NewTasks = [...tasks]
+		NewTasks.splice(index, 1)
+		setTasks(NewTasks)
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		
+		<div className="container-fluid d-flex justify-content-center" >
+			<h1>ToDos
+			</h1>
+	<ul className="blurred-box" id="card">
+	<li className="login-box d-flex justify-content-between align-items-center">
+		<input className="user-password" type="text" onKeyDown={e=>addTask(e)} onChange={e=>setNewTask(e.target.value)} value={newTask} name="task" id="task" />
+	</li>
+		{tasks.map((task, index)=>(
+  <li key={index} className="login-box d-flex justify-content-between align-items-center" id="listItem">
+    {task}
+    <span onClick={removeTask} className="badge rounded-pill" id="lax">X</span>
+  </li>
+		))}
+		<li className="list-group-item text-center d-flex  justify-content-center align-items-center" id="items"><small>{tasks.length} items.</small></li>
+</ul>
+	
+  
+    </div>
+	
+	
 	);
+	
 };
 
 export default Home;
